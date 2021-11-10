a60ab1b4c0a3b2162f9d71613bf30ba50984d785
ellsonmendesyt <elsonmendesoficial@gmail.com>
2021-11-10 08:05:36 -0300

    pronto

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
 
-import Home from './pages/Home'
-import theme from  './theme'
-import {ThemeProvider} from 'styled-components'
-import {Reset} from 'styled-reset'
-
-
+import Home from './pages/Home';
+import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Reset/>
-      <Home/>
+      <Reset />
+      <Home />
     </ThemeProvider>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,13 @@
-const theme={
-    colors:{
-        primary:'#6200ee',
-        background:'#00000014',
-     
-        text:'#000000b3',
-        success:'#135e01'
-    },
-    fonts:{regular:'Roboto, sans-serif',},
+const theme = {
+  colors: {
+    primary: '#6200ee',
+    background: '#00000014',
+    text: '#000000B3',
+    success: '#135E01',
+  },
+  fonts: {
+    fontFamily: 'Roboto, sans-serif',
+  },
 };
 
 export default theme;
